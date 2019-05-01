6716c01d2fb34cfbef012fae7a0333be94099807
Artur <elkin.as@mail.ru>
2019-05-01 17:36:55 +0300

    adding implementation of create new tasks

--- src/components/item-add-form.jsx
+++ src/components/item-add-form.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 
 class ItemAddForm extends React.Component {
+  state = {
+    taskTitle: ""
+  };
+
+  onLabelChange = event => {
+    this.setState({
+      taskTitle: event.target.value
+    });
+  };
+  onSubmit = event => {
+    event.preventDefault();
+    this.props.onItemAdded(this.state.taskTitle);
+    this.setState({
+      taskTitle: ''
+    })
+  };
   render() {
     return (
-      <div className="row mt-4">
+      <form 
+        className="row mt-4"
+        onSubmit={this.onSubmit}
+        >
         <input
           type="text"
           placeholder="Task title"
           className="col-10 col-sm-10 search-panel"
+          onChange={this.onLabelChange}
+          value={this.state.taskTitle}
         />
-        <button
-          className="btn btn-outline-secondary col-2"
-          onClick={() => this.props.onItemAdded("helloContent")}
-        >
-          Add task
-        </button>
-      </div>
+        <button className="btn btn-outline-secondary col-2">Add task</button>
+      </form>
     );
   }
 }
